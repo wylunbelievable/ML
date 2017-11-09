68ebdf2e4102b7a0dcc7293d8cacfc9e5681a760
wylunbelievable <wylluanqibazao@163.com>
2017-11-09 21:28:43 +0800

    decision_tree mini project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -18,13 +18,19 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
+print 'The number of the features are ', len(features_train[0])
 
 
 
 #########################################################
 ### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_test)
+print 'The accuracy is ', accuracy_score(labels_test,pred)
 
 #########################################################
 
