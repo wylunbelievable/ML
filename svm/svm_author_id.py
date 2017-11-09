79aedb02d6855b1404f50a60cc3e67bdd47d5ee3
wylunbelievable <wylluanqibazao@163.com>
2017-11-09 21:29:05 +0800

    SVM mini project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from collections import Counter
+
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100] 
+
+clf = SVC(C=10000.0, kernel = "rbf" )
+t0 = time()
+clf.fit(features_train,labels_train)
+print 'The training time is ', round(time()-t0,3),'s'
+t0 = time()
+pred = clf.predict(features_test)
+print 'The predicting time is ',round(time()-t0,3),'s'
+
+print 'The accuracy is ', accuracy_score(labels_test, pred)
+
+print 'There are ',Counter(pred), "belong to 'Chris'" 
 
 #########################################################
 
